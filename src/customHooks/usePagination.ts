bb9c21464b15a7194dfc969c1f0c766ebf186ffc
4drea4 <bakerand@outlook.com>
2025-12-16 14:42:24 -0600

    Setting up files and installing packages

---
+++ src/customHooks/usePagination.ts
@@ -0,0 +1,3 @@
+//inupts: total items, items perpage, initial page
+//Output: currentPage, totalPages/items per page
+//startindex, endIndex , items on current page, set page, next page, prev page, can next page, can prev page
