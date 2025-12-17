d5fa69ca5837adc14514c1485ef2fd6f4ba9fbdf
4drea4 <bakerand@outlook.com>
2025-12-17 09:26:51 -0600

    Gloryyyyyyyy putting this on pause

--- src/customHooks/usePagination.ts
+++ src/customHooks/usePagination.ts
@@ -2,16 +2,20 @@
 //Output: currentPage, totalPages/items per page
 //startindex, endIndex , items on current page, set page, next page, prev page, can next page, can prev page
 
-import { useState} from 'react';
+import { useMemo, useState} from 'react';
 import type { UsePagination, UsePaginationResults } from '../types';
 
-export function UsePagination({
-    items,
+// custom hook , destructuring, 
+export function usePagination({
+    // default values
+    totalItems,
     itemsPerPage = 10,
     initialPage = 1,
+  
 }) : UsePagination ): UsePaginationResults {
-    const totalPages = useMemo (
-        () => Math.max(1, Math.ceil(items/ itemsPerPage )),
-        [items, itemsPerPage]
-    );
-}
+    // setting inputs and outputs
+    //how many pages we need and then redo if itemsPerPage or totalItems changes
+    const totalPages = useMemo (() => {
+        return Math.max(1, Math.ceil(totalItems/ itemsPerPage ));
+},
+        [totalItems, itemsPerPage]);
